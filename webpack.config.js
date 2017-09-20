8c5b901ba5adfa7d22f9af6e345d1fb35f164b94
Rongduan Zhu <rongduan.zhu@gmail.com>
2017-09-20 00:07:29 -0700

    react skeleton app

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,14 @@ var config = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
   plugins: [new HtmlWebpackPlugin({
-      title: 'Tic Tac Toe'
+      template: path.join(__dirname, 'src', 'index.html')
   })]
 };
 
