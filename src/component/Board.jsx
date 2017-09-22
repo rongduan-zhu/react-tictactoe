f7d79937dd1fc5db7de55e7855b774f3aedc83a6
Rongduan Zhu <rongduan.zhu@gmail.com>
2017-09-22 00:33:36 -0700

    clickable board with players

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -2,16 +2,54 @@ import React from 'react';
 import { Square } from './Square.jsx';
 
 export class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = this.init();
+    }
+
+    init() {
+        return {
+            squares: Array(9).fill(undefined),
+            player: 'X',
+            winner: ''
+        };
+    }
+
+    handleClick(id) {
+        if (this.state.squares[id]) {
+            return;
+        }
+
+        let newSquares = this.state.squares.slice();
+        let newPlayer = this.nextPlayer(this.state.player);
+
+        newSquares[id] = this.state.player;
+
+        this.setState({
+            squares: newSquares,
+            player: newPlayer
+        });
+    }
+
+    nextPlayer(state) {
+        return state === 'X' ? 'O' : 'X';
+    }
+
+    reset() {
+        this.setState(this.init());
+    }
+
     renderSquare(id) {
         return (
-            <Square value={id} />
+            <Square value={this.state.squares[id]}
+                onClick={() => this.handleClick(id)}/>
         );
     }
 
     renderRow(baseId) {
         return (
             <div className="row">
-                {[...new Array(3)].map((_, i) => (this.renderSquare(baseId + i)))}
+                {[...Array(3)].map((_, i) => (this.renderSquare(baseId + i)))}
             </div>
         )
     }
@@ -19,7 +57,10 @@ export class Board extends React.Component {
     render() {
         return (
             <div>
-                {[...new Array(3)].map((_, i) => (this.renderRow(i * 3)))}
+                <h1>Current Player: {this.state.player}</h1>
+                <h1>Next Player: {this.nextPlayer(this.state.player)}</h1>
+                <button onClick={() => this.reset()}>Reset</button>
+                {[...Array(3)].map((_, i) => (this.renderRow(i * 3)))}
             </div>
         );
     }
