7c67f7a853bdf2cdaad5264f1e4eec3286dec059
Rongduan Zhu <rongduan.zhu@gmail.com>
2017-09-23 02:11:19 -0700

    check for winner

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Square } from './Square.jsx';
+import { find } from 'lodash';
 
 export class Board extends React.Component {
     constructor() {
@@ -11,7 +12,8 @@ export class Board extends React.Component {
         return {
             squares: Array(9).fill(undefined),
             player: 'X',
-            winner: ''
+            winner: '',
+            finished: false
         };
     }
 
@@ -25,10 +27,37 @@ export class Board extends React.Component {
 
         newSquares[id] = this.state.player;
 
+        let winner = this.checkWinner(newSquares);
+
         this.setState({
             squares: newSquares,
-            player: newPlayer
+            player: newPlayer,
+            winner: winner,
+            finished: !!winner
         });
+    }
+
+    checkWinner(squares) {
+        let states = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        let winningState = find(
+            states,
+            state => !!state[0] && squares[state[0]] == squares[state[1]] && squares[state[1]] == squares[state[2]]
+        );
+
+        if (!winningState) {
+            return '';
+        }
+
+        return squares[winningState[0]];
     }
 
     nextPlayer(state) {
@@ -59,6 +88,7 @@ export class Board extends React.Component {
             <div>
                 <h1>Current Player: {this.state.player}</h1>
                 <h1>Next Player: {this.nextPlayer(this.state.player)}</h1>
+                <h1>Winner is: {this.state.winner}</h1>
                 <button onClick={() => this.reset()}>Reset</button>
                 {[...Array(3)].map((_, i) => (this.renderRow(i * 3)))}
             </div>
